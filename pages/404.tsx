2fe8384911a65b33fb31f5368185a18df028d95c
RenPc <sudahjuga25@gmail.com>
2022-12-01 03:44:00 +0700

    auto dialihkan ke home jika user ke 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,6 +1,15 @@
 import style from '../styles/404.module.css'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 
 export default function custom404() {
+  const router = useRouter();
+  useEffect(() => {
+  setTimeout(() => {
+    router.push('/')
+  }, 5000)
+  }, [])
+  
   return (
     <div className={style.main}>
         <div className={style.kode}>404</div>
@@ -9,6 +18,8 @@ export default function custom404() {
         kamu sepertinya nyasar?
         <br />
         aku balikin ke home yak 
+        <br />
+        tunggu yak, nyari ojek dulu
     </div>
   )
 }
