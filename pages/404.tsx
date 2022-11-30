154a5c1de61e01514d16504847c2ace464fa266f
RenPc <sudahjuga25@gmail.com>
2022-12-01 02:37:31 +0700

    penambahan navbar dan custom 404

---
+++ pages/404.tsx
@@ -0,0 +1,14 @@
+import style from '../styles/404.module.css'
+
+export default function custom404() {
+  return (
+    <div className={style.main}>
+        <div className={style.kode}>404</div>
+        🙄
+        <br />
+        kamu sepertinya nyasar?
+        <br />
+        aku balikin ke home yak 
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,19 @@
 import Image from "next/image";
+import Navbar from "./komponen/navbar";
+import ImageBg from "./komponen/imagebg";
 
 export default function index (){
     return (
         <div>
             <title>Ren Profiles</title>
             <link rel="icon" href="/favicon.ico" />
-            
-            <h1 className="text-3xl font-bold">
-      Segera Datang
-    </h1>
-    <br />
-    <h2><a href="https://www.instagram.com/muhamad_reno1215/">My Instagram Account</a></h2>
-    <h2>Powered By Next Js</h2>
+            <Navbar />
+            <ImageBg />
+            <h1 className="text-3xl font-bold">Coming Soon</h1>
+            <br />
+            <h2><a href="https://www.instagram.com/muhamad_reno1215/">My Instagram Account</a></h2>
+            <h2>Powered By Next Js</h2>
+            <Image src='/hina.png' alt="hikawa hina" width={1334} height={1002}/>
         </div>
     );
 };
