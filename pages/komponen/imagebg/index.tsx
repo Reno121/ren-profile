019f0162f90a839e1047f80799f5000851d520fc
RenPc <sudahjuga25@gmail.com>
2022-12-01 02:47:25 +0700

    penambahan folder

---
+++ pages/komponen/imagebg/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Image from 'next/image';
+
+export default function ImageBg (){
+    return (
+        <div>
+            <Image src='/udagawa-ako.png' alt='ako-bg' width={1500} height={1500} />
+        </div>
+    );
+};
