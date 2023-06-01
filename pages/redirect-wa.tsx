d282a0a0be37ac197118790869c3050db8a7498e
RenPc <sudahjuga25@gmail.com>
2023-06-01 13:44:18 +0700

    nonaktifkan eslint anj ama matiin wa

--- pages/redirect-wa.tsx
+++
@@ -1,9 +0,0 @@
-import { NextApiHandler } from 'next';
-
-const handler: NextApiHandler = (req, res) => {
-  const redirectUrl = 'https://wa.me/6285811741360'; // ganti dengan URL halaman tujuan di situs lain
-  res.writeHead(302, { Location: redirectUrl });
-  res.end();
-};
-
-export default handler;
